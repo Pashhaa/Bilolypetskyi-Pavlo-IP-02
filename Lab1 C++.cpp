1655497b964a4e9feebab7f26a69193389815757
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2020-10-24 23:59:46 +0300

    Add files via upload

---
+++ Lab1 C++.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float x,p = 3.14159265;
+	cout << "Градусна мiра:";
+	cin >> x; 
+	cout << "Радiанна мiра:" << x*p/180;
+	system("pause>nul");
+}
