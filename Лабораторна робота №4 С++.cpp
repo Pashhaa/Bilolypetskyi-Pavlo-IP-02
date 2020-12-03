fb5585aff88341679a5d11b5ae4bfdf64d9f6f30
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2020-12-03 12:18:25 +0300

    Add files via upload

---
+++ Лабораторна робота №4 С++.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int n, i;
+	float a = 2;
+	float s = 0;
+
+
+	cout << "Введiть бажану для обчислення кiлькiсть членiв: ";
+
+	cin >> n;
+
+	if (n < 0)
+	{
+		cout << "Недопустиме значення n";
+
+	}
+	else
+	{
+	
+
+	for (i = 1; i <= n; i++ )
+	{
+		a = pow(a, 2) / (a + 3);
+		s += a;
+		}
+	cout << s;
+}
+	return 0;
+}
